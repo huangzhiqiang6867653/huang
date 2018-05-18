e04bf744f3fb5820a34937f7a86ccbaa5ed99f89
huangzhiqiang <15855172401@163.com>
2018-05-18 10:30:53 +0800

    初始化脚本

--- vendor/table_modify.sql
+++ vendor/table_modify.sql
@@ -15,3 +15,46 @@ create table tb_relation_role_fd
 auto_increment = 1;
 alter table tb_relation_role_fd comment '角色权限关联表';
 
+/*==============================================================*/
+/* 修改表time为 datetime                                         */
+/*==============================================================*/
+ALTER TABLE tb_business_user MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_user MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_user MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_user SET delete_flag = NULL;
+
+ALTER TABLE tb_business_dictionary MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_dictionary MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_dictionary MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_dictionary SET delete_flag = NULL;
+
+ALTER TABLE tb_business_dictionary_detail MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_dictionary_detail MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_dictionary_detail MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_dictionary_detail SET delete_flag = NULL;
+
+ALTER TABLE tb_business_function MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_function MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_function MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_function SET delete_flag = NULL;
+
+ALTER TABLE tb_business_function_detail MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_function_detail MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_function_detail MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_function_detail SET delete_flag = NULL;
+
+ALTER TABLE tb_business_group MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_group MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_group MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_group SET delete_flag = NULL;
+
+ALTER TABLE tb_business_organization MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_organization MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_organization MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_organization SET delete_flag = NULL;
+
+ALTER TABLE tb_business_role MODIFY COLUMN create_time datetime comment '创建时间';
+ALTER TABLE tb_business_role MODIFY COLUMN update_time datetime comment '更新时间';
+ALTER TABLE tb_business_role MODIFY COLUMN delete_flag int(11) DEFAULT NULL comment '删除标记(默认为空)';
+UPDATE tb_business_role SET delete_flag = NULL;
+
